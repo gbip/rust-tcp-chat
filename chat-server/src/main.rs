038c6dbaed661e65caac24d16291a55fa5708b54
Paul Florence <florencepaul@hotmail.fr>
2016-11-10 20:05:57 +0100

    Gestion des erreurs, et ajout d'un message d'accueil.

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -3,7 +3,6 @@ use std::vec::Vec;
 use std::string::String;
 use std::io::Write;
 
-
 struct Application {
     clients : std::vec::Vec<TcpStream>,
 }
@@ -14,7 +13,7 @@ impl Application {
 
         for mut client in &self.clients {
         
-            client.write_fmt(format_args!("{}",message));
+            client.write_fmt(format_args!("{}",message)).expect("Internal error");
 
         }
     }
@@ -22,9 +21,9 @@ impl Application {
     fn add_client(&mut self, client : TcpStream) {
         
         self.clients.push(client);
-        println!("New client connected");        
+        println!("New client connected");
+        self.publish(String::from("A client has just connected"));
     }
-
 
 
 }
@@ -34,10 +33,9 @@ fn main() {
     let mut app = Application{clients : Vec::new()};
     let listener = TcpListener::bind("127.0.0.1:80").unwrap();
     for stream in listener.incoming() {
-        //let mut ref_app = &app;
         match stream {
             Ok(stream) => {app.add_client(stream);}
-            Err(e) => println!("Connection refused from a client"),
+            Err(e) => println!("Connection refused from a client : {}", e),
         }
     }
 
