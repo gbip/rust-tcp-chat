59e4e6c0cea39398acd3730f7fb6438155e5a905
Paul Florence <florencepaul@hotmail.fr>
2016-11-10 20:00:33 +0100

    Implémentation du serveur. Fonctions de base.

---
+++ chat-server/src/main.rs
@@ -0,0 +1,45 @@
+use std::net::{TcpListener, TcpStream};
+use std::vec::Vec;
+use std::string::String;
+use std::io::Write;
+
+
+struct Application {
+    clients : std::vec::Vec<TcpStream>,
+}
+
+impl Application {
+
+    fn publish(&self, message : String) {
+
+        for mut client in &self.clients {
+        
+            client.write_fmt(format_args!("{}",message));
+
+        }
+    }
+
+    fn add_client(&mut self, client : TcpStream) {
+        
+        self.clients.push(client);
+        println!("New client connected");        
+    }
+
+
+
+}
+
+fn main() {
+    println!("Server chat");
+    let mut app = Application{clients : Vec::new()};
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    for stream in listener.incoming() {
+        //let mut ref_app = &app;
+        match stream {
+            Ok(stream) => {app.add_client(stream);}
+            Err(e) => println!("Connection refused from a client"),
+        }
+    }
+
+
+}
